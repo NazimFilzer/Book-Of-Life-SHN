d53e415a547aff3f7566fa1c8f66f3b299da3f4c
jaison080 <jaisondennis080@gmail.com>
2022-09-16 18:37:32 +0530

    Updated

--- book-of-life/pages/dashboard.js
+++ book-of-life/pages/dashboard.js
@@ -1,4 +1,4 @@
-import { Button, TextField } from "@mui/material";
+import { Box, Button, Modal, TextField, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import supabaseClient from "../utils/supabaseClient";
 import styles from '../styles/Dashboard.module.css';
@@ -8,6 +8,9 @@ import { useRouter } from "next/router";
 export default function Dashboard() {
 
 const router=useRouter();
+const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   async function signout() {
     const { error } = await supabaseClient.auth.signOut()
     router.push('/')
@@ -28,7 +31,17 @@ const router=useRouter();
     setPosts(data)
   }
 
-  
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
   function getRandomColor() {
     var letters = 'BCDEF'.split('');
     var color = '#';
@@ -47,15 +60,31 @@ const router=useRouter();
         <Link href='/create'><Button variant="contained">Create POST</Button></Link>
         <div className={styles.posts_container}>
           {posts.map(post => (
-            <div key={post.id} style={{ backgroundColor: getRandomColor(), width: "400px", padding: "10px", borderRadius: "5px", border: "1px solid", boxShadow: "5px 10px #888888" }}>
+            <div key={post.id} onClick={handleOpen} style={{ backgroundColor: getRandomColor(), width: "400px", padding: "10px", borderRadius: "5px", border: "1px solid", boxShadow: "5px 10px #888888" }}>
               <h3>{post.created_at.substring(0, 10)} </h3>
               <p>{post.content} </p>
+             
             </div>
-          ))
-
-          }
+           
+          ))}
+        
         </div>
       </div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
     </>
   )
 }
