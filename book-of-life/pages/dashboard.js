c42f638b52d74fe6837620c84a96fb5be6df9351
jam-dev <jamalvga2002@gmail.com>
2022-09-17 21:42:08 +0530

    nav created

--- book-of-life/pages/dashboard.js
+++ book-of-life/pages/dashboard.js
@@ -4,6 +4,7 @@ import supabaseClient from "../utils/supabaseClient";
 import styles from '../styles/Dashboard.module.css';
 import Link from 'next/link'
 import Head from 'next/head'
+import Image from 'next/image'
 import { useRouter } from "next/router";
 import dynamic from 'next/dynamic'
 import { red} from '@mui/material/styles'
@@ -81,11 +82,20 @@ function Dashboard() {
         <title>Book Of Life | Dashboard</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <div className={styles.dashboard_container}>Dashboard
-        <Button variant="contained" >{User?.user_metadata.name}</Button>
-        <Button variant="contained" style={{ backgroundColor:"#8D0000" }} onClick={signout}>Signout</Button>
-        <Link href='/create'><Button variant="contained">Create POST</Button></Link>
-        <div className={styles.posts_container}>
+      <div className={styles.dashboard_container}>
+
+      <div className={styles.dashboard_nav} >
+         <div className={style.dashboard_nav_logo}>
+         <Image src="/logo.png" width="150" height="150" />
+         </div>
+        <Button variant="contained" style={{ backgroundColor:"#fff", color:"#000" }} className={styles.dashboard_button}><Image style={{ padding:"10px" }} className={styles.dashboard_button_img} src="/profile.png" width="20" height="20" />{User?.user_metadata.name}</Button>
+        <Button variant="contained" style={{ backgroundColor:"#fff" , color:"#000"}} onClick={signout} className={styles.dashboard_button}><Image style={{ padding:"10px" }}src="/logout.png" width="20" height="20" />Signout</Button>
+        <Link href='/create'><Button variant="contained" style={{ backgroundColor:"#fff", color:"#000" }} className={styles.dashboard_button}><Image style={{ padding:"10px" }}src="/create.png" width="20" height="20" />Create POST</Button></Link>
+
+      </div>
+      <div className={styles.dashboard_content}>
+      <div className={styles.posts_container}>
+
           {posts.map(post => (
             <div key={post.id} style={{ backgroundColor: getRandomColor(), width: "300px", padding: "10px", borderRadius: "5px", border: "1px solid", boxShadow: "5px 10px #888888" }}>
               <h3 style={{ cursor: "pointer" }} onClick={() => { handleOpen(); setmodalData(post) }} >{post.created_at.substring(0, 10)} </h3>
@@ -93,25 +103,20 @@ function Dashboard() {
               <Button variant="contained" style={{ backgroundColor:"#8D0000" }} onClick={() => { deletePost(post.id) }} props={post} >Delete</Button>
             </div>
 
+
+
           ))}
 
         </div>
+
       </div>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            {modalData ? modalData.created_at.substring(0, 10) : ""}
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            {modalData.content}
-          </Typography>
-        </Box>
-      </Modal>
+      
+      
+      Dashboard
+        
+   
+      </div>
+      
     </>
   )
 }
